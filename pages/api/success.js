f76104ef516ff36246b925776279856dbf9eff74
Rahim <rehim.firudunlu1997@gmail.com>
2024-03-30 21:34:46 +0400

    change status code

--- pages/api/success.js
+++ pages/api/success.js
@@ -1,6 +1,4 @@
 export default function handler(req, res) {
-  if (req.method === 'POST') {
-    const { params } = req.query;
-    return res.redirect(304, `https://vincode.az/success?params=${params}`);
-  }
+  const { params } = req.query;
+  return res.redirect(304, `https://vincode.az/success?params=${params}`);
 }
--- pages/api/topup/success.js
+++ pages/api/topup/success.js
@@ -1,4 +1,4 @@
 export default function handler(req, res) {
   const { params } = req.query;
-  res.redirect(200, `https://vincode.az/success?params=${params}`);
+  res.redirect(304, `https://vincode.az/success?params=${params}`);
 }
