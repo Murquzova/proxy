24936c0a8fca8448a687fd626190377e6ea5eb38
Rahim <rehim.firudunlu1997@gmail.com>
2024-03-30 22:00:45 +0400

    change status code

--- pages/api/decline.js
+++ pages/api/decline.js
@@ -1,3 +1,3 @@
 export default function handler(req, res) {
-  res.redirect(304, `https://vincode.az/decline`);
+  res.redirect(307, `https://vincode.az/decline`);
 }
--- pages/api/index.js
+++ pages/api/index.js
@@ -1,3 +1,3 @@
 export default function handler(req, res) {
-  res.redirect(304, 'https://vincode.az');
+  res.redirect(307, 'https://vincode.az');
 }
